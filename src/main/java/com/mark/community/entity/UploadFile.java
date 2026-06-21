5dc597390090eddfd8eadd39a54a48cc163c3477
juseungjuseung <juseungjuseung@gmail.com>
2026-06-21 17:58:34 +0900

    refactor: UploadFile 엔티티 설정 추가 및 파일타입 열거형으로 처리로 변경

--- src/main/java/com/mark/community/entity/UploadFile.java
+++ src/main/java/com/mark/community/entity/UploadFile.java
@@ -1,30 +1,28 @@
 package com.mark.community.entity;
 
+import com.mark.community.enums.FileType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class UploadFile {
-    private String fileId;
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fileName;
     private String filePath;
-    private String fileType;
 
+    @Enumerated(EnumType.STRING)
+    private FileType fileType;
+    private Long fileSize;
 
-    public UploadFile(String fileId, String filePath, String fileType){
-        this.fileId = fileId;
+    public UploadFile(String fileName, String filePath, FileType fileType, Long fileSize) {
+        this.fileName = fileName;
         this.filePath = filePath;
         this.fileType = fileType;
-    }
-
-    public UploadFile(){
-
-    }
-
-    public String getFileId() {
-        return fileId;
-    }
-
-    public String getFilePath() {
-        return filePath;
-    }
-
-    public String getFileType() {
-        return fileType;
+        this.fileSize = fileSize;
     }
 }
