4218928733dc6584e7cd33999b92ccd3af4b24d8
juseungjuseung <juseungjuseung@gmail.com>
2026-06-21 16:51:02 +0900

    refactor: 댓글 엔티티 연관관계 추가

--- src/main/java/com/mark/community/entity/Comment.java
+++ src/main/java/com/mark/community/entity/Comment.java
@@ -1,84 +1,41 @@
 package com.mark.community.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "comments")
 public class Comment {
-    private String commentId;
-    private String postId;
-    private String userId;
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
     private String comment;
-    private String nickname;
     private boolean deleted = false;
-    private String parentCommentId;
+    private Long parentCommentId;
     private Date commentTime;
 
 
-    public Comment(String postId, String userId, String comment, Date commentTime, String nickName, String parentCommentId){
-        this.postId = postId;
-        this.userId = userId;
+    public Comment(Post post, User user, String comment, Date commentTime, Long parentCommentId) {
+        this.post = post;
+        this.user = user;
         this.comment = comment;
         this.commentTime = commentTime;
-        this.nickname = nickName;
-        this.parentCommentId = parentCommentId;
-
-    }
-
-    public Comment(){
-
-    }
-
-    public String getCommentId() {
-        return commentId;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public boolean isDeleted() {
-        return deleted;
-    }
-
-    public String getParentCommentId() {
-        return parentCommentId;
-    }
-
-    public String getPostId() {
-        return postId;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public Date getCommentTime() {
-        return commentTime;
-    }
-
-    public void setCommentId(String commentId) {
-        this.commentId = commentId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public void setDeleted(boolean deleted) {
-        this.deleted = deleted;
-    }
-
-    public void setParentCommentId(String parentCommentId) {
         this.parentCommentId = parentCommentId;
     }
+
 
     public void setComment(String comment) {
         this.comment = comment;
@@ -86,5 +43,9 @@ public class Comment {
 
     public void setCommentTime(Date commentTime) {
         this.commentTime = commentTime;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
     }
 }
