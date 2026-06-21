fb54ff4a65fa91468070ec721bda31907e673a23
juseungjuseung <juseungjuseung@gmail.com>
2026-06-21 17:58:46 +0900

    refactor: UploadFile 레포지토리 추가

---
+++ src/main/java/com/mark/community/repository/UploadFileRepository.java
@@ -0,0 +1,10 @@
+package com.mark.community.repository;
+
+import com.mark.community.entity.UploadFile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UploadFileRepository extends JpaRepository<UploadFile, Long> {
+
+}
