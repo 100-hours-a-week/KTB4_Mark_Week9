379ce269d8ffbb351b415293fd776a173807c0d9
juseungjuseung <juseungjuseung@gmail.com>
2026-06-26 15:37:28 +0900

    feat: upload 폴더 경로 매칭을 위한 addResourceHandlers 추가

--- src/main/java/com/mark/community/config/WebConfig.java
+++ src/main/java/com/mark/community/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.mark.community.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,5 +14,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT","PATCH", "DELETE") // HTTP 메서드 허용
                 .allowedHeaders("*")                                    // 모든 헤더 허용
                 .allowCredentials(true);                                // 쿠키 인증 요청 허용
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:uploads/");
     }
 }
