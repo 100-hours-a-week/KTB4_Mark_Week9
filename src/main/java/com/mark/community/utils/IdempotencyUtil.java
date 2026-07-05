4c114d7aceee8be0d0afb5c8943b09e91d156686
juseungjuseung <juseungjuseung@gmail.com>
2026-07-06 00:09:34 +0900

    refactor: 멱등키 동시성 문제 해결을 위해 ConcurrentHashMap으로 변경 및 10분 지난 요청 멱등키 저장소에서 삭제 로직 추가

--- src/main/java/com/mark/community/utils/IdempotencyUtil.java
+++ src/main/java/com/mark/community/utils/IdempotencyUtil.java
@@ -1,19 +1,31 @@
 package com.mark.community.utils;
 
-import com.mark.community.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 public class IdempotencyUtil {
-    private static Map<String, ResponseEntity<?>> idempotencyKeys = new HashMap<>();
+    private static Map<String, ResponseEntity<?>> idempotencyKeys = new ConcurrentHashMap<>();
+    private static final Map<String, Long> expireTimes = new ConcurrentHashMap<>();
+    private static final long EXPIRE_TIME = TimeUnit.MINUTES.toMillis(10);
 
     public static void setResponse(String idempotencyKey , ResponseEntity<?> responseEntity){
+        if(idempotencyKey == null || idempotencyKey.isBlank()) return;
         idempotencyKeys.put(idempotencyKey, responseEntity);
+        expireTimes.put(idempotencyKey, System.currentTimeMillis() + EXPIRE_TIME);
     }
 
     public static ResponseEntity<?> getResponse(String idempotencyKey){
+        if(idempotencyKey == null || idempotencyKey.isBlank()) return null;
+        Long expire = expireTimes.get(idempotencyKey);
+        if(expire != null && expire < System.currentTimeMillis()) {
+            idempotencyKeys.remove(idempotencyKey);
+            expireTimes.remove(idempotencyKey);
+            return null;
+        }
+
         return idempotencyKeys.get(idempotencyKey);
     }
 }
