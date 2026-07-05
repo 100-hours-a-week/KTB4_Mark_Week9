fded342f4feae6a5673cb2ab88902f76b24d7438
juseungjuseung <juseungjuseung@gmail.com>
2026-07-05 21:55:47 +0900

    refactor: 로그인 책임 Spring Security로 위임 후 필요없는 로직 및 AuthService 삭제

--- src/main/java/com/mark/community/controller/AuthController.java
+++ src/main/java/com/mark/community/controller/AuthController.java
@@ -2,7 +2,6 @@ package com.mark.community.controller;
 
 import com.mark.community.messages.ApiResponseMessage;
 import com.mark.community.response.ApiResponse;
-import com.mark.community.service.AuthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.csrf.CsrfToken;
@@ -11,12 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
-    private final AuthService authService;
-
-    public AuthController(AuthService authService){
-        this.authService = authService;
-    }
-
     @GetMapping("/csrf")
     public ResponseEntity<?> csrfToken(CsrfToken csrfToken){
         csrfToken.getToken();
