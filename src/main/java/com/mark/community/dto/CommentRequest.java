ef23df7317e097ea4d173ee315149f33946c5f38
juseungjuseung <juseungjuseung@gmail.com>
2026-07-07 08:52:05 +0900

    feat: 테스트 코드 작성을 위해 생성자 추가

--- src/main/java/com/mark/community/dto/CommentRequest.java
+++ src/main/java/com/mark/community/dto/CommentRequest.java
@@ -1,8 +1,16 @@
 package com.mark.community.dto;
 
+import lombok.Getter;
+
+@Getter
 public class CommentRequest {
     private String comment;
     private Long parentCommentId;
+
+    public CommentRequest(String comment, Long parentCommentId) {
+        this.comment = comment;
+        this.parentCommentId = parentCommentId;
+    }
 
     public String getComment() {
         return comment;
