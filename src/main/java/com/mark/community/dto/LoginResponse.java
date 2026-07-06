f4a55da9b41379c70e76e672013901fb1c1bf223
juseungjuseung <juseungjuseung@gmail.com>
2026-07-07 08:52:19 +0900

    feat: 사용자 권한 반환을 위한 필드 추가

--- src/main/java/com/mark/community/dto/LoginResponse.java
+++ src/main/java/com/mark/community/dto/LoginResponse.java
@@ -6,9 +6,11 @@ import lombok.Getter;
 public class LoginResponse {
     private Long profileFileId;
     private Long userId;
+    private String userRole;
 
-    public LoginResponse(Long profileFileId, Long userId){
+    public LoginResponse(Long profileFileId, Long userId, String userRole){
         this.profileFileId = profileFileId;
         this.userId = userId;
+        this.userRole = userRole;
     }
 }
